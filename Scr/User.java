91584bf944c3fb276d0171339082612e252e883e
Tristan Thompson <125337721+Tristan-Thompson876@users.noreply.github.com>
2023-04-10 15:06:48 -0500

    Update User.java

--- Scr/User.java
+++ Scr/User.java
@@ -1,5 +1,28 @@
 package Scr;
 
 public class User {
+    private int id;
+    private int score;
+    private int accuracy=0;
+
+    public User(String name,int id,int score, int accuracy){
+        this.name = name;
+        this.id = id;
+        this.score = score;
+        this.accuracy = accuracy;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public int score(){
+        return score;
+    }
+    public int getAccuracy(){
+        return accuracy;
+    }
+
     
 }
