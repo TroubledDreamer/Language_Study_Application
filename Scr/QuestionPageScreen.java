886bbc3d9a5d8be22b75d9a7ab83d639add369d6
Tristan Thompson <125337721+Tristan-Thompson876@users.noreply.github.com>
2023-04-13 10:49:02 -0500

    lil smth

--- Scr/QuestionPageScreen.java
+++ Scr/QuestionPageScreen.java
@@ -1,15 +1,22 @@
 package Scr;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class QuestionPageScreen{
     
     private JPanel ScrGrade;
-    JPanel Question = new JPanel();
+    JPanel QPanel = new JPanel();
     JPanel ScrScreen = new JPanel();
     JButton submit = new JButton();
 
@@ -30,6 +37,27 @@ public class QuestionPageScreen{
             catch(){
                 
             }
+        }
+    }
+
+    private class questionDisplay{
+        for(String question: questionList){
+            JPanel questionPanel = new JPanel(new BorderLayout());
+            questionPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+            // Add question text
+            JLabel questionLabel = new JLabel(question);
+            questionPanel.add(questionLabel, BorderLayout.NORTH);
+
+            // Add answer choices
+            ButtonGroup buttonGroup = new ButtonGroup();
+            for (String answer : questions.get(question)) {
+                JRadioButton radioButton = new JRadioButton(answer);
+                buttonGroup.add(radioButton);
+                questionPanel.add(radioButton);
+            }
+
+            QPanel.add(questionPanel);
         }
     }
 
